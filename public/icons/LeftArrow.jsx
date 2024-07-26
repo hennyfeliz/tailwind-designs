c565394eaab212777beba3a5af9c607b3cd70564
Henny Feliz <mingito.2017@gmail.com>
2024-07-25 22:41:31 -0400

    adding left arrow icon component

---
+++ public/icons/LeftArrow.jsx
@@ -0,0 +1,3 @@
+export default function LeftIcon() {
+  return <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+}
