8e6a88a22228b7e1dc0b4684dc65490069f3ddb1
Henny Feliz <mingito.2017@gmail.com>
2024-07-26 00:48:56 -0400

    full app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,104 @@
-/* eslint-disable react/no-unknown-property */
-import './App.css'
+/* src/App.jsx */
+import SongIcon1 from "../public/icons/SongIcon1";
+import SongIcon2 from "../public/icons/SongIcon2";
+import SongIcon3 from "../public/icons/SongIcon3";
+import LeftArrow from "../public/icons/LeftArrow";
+import RightArrow from "../public/icons/RightArrow";
+import Play from "../public/icons/Play";
+import Pause from "../public/icons/Pause";
+import "./App.css";
+import { useEffect, useState } from "react";
+import SongPf from "../public/icons/SongPf";
 
 function App() {
 
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [thumb, setThumb] = useState(0);
+
+  useEffect(() => {
+
+    const slider = document.getElementById('slider');
+
+    function updateBackground() {
+      const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+      slider.style.setProperty('--range-progress', `${value}%`);
+    }
+
+    slider.addEventListener('input', updateBackground);
+    updateBackground();
+
+    setTimeout(() => {
+      setThumb(thumb + 1);
+    }, 1000);
+  }, [thumb]);
+
   return (
-    <>
-      <div className='flex justify-between bg-slate-50 shadow h-4 items-center px-4'>
-        <div>
-          <button className='rounded p-1 text-slate-500 transition-colors hover:bg-sky-500 hover:text-slate-100 focus:ring-2 focus:ring-slate-200'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-          </button>
+    <div className="flex justify-center items-center bg-[#40b4d2] h-screen w-full">
+
+      {/* HEADER CONTAINER */}
+      <div className="w-[340px] h-[370px] bg-white rounded-lg">
+
+        {/* HEADER */}
+        <div className="flex justify-between items-center px-4 h-10 font-semibold">
+
+          {/* HEADER TITLE */}
+          <span>Music Player</span>
+
+          {/* HEADER ICONS */}
+          <div className="flex items-center space-x-1">
+            <SongIcon1 />
+            <SongIcon2 />
+            <SongIcon3 />
+          </div>
         </div>
-        <div className='flex p-1 space-x-4'>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
-            </svg>
-          </button>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
-            </svg>
-          </button>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-            </svg>
-          </button>
+        {/* PLAYER CONTAINER */}
+        <div className="flex flex-row px-4 w-full h-20 items-center space-x-2">
+          <div className="w-16 h-16 bg-gray-200 rounded flex items-center justify-center">
+            <SongPf />
+          </div>
+          <div>
+            <h1 className="font-semibold text-xl">Blowin in the Wind</h1>
+            <span className="text-gray-400">Bob Dylan</span>
+          </div>
         </div>
+        {/* CONTROLS */}
+        <div className="w-full h-8 border px-4 items-center">
+          {/* <input className="w-full h-6 opacity-70 outline-none bg-black" type="range" min="1" max="100" value="20" id="myRange" /> */}
+          <input className="" type="range" min="1" max="60" value={thumb} id="slider" />
+        </div>
+
+        {/* LOW CONTROLS */}
+        <div className="flex w-full h-14 px-20 items-center justify-between">
+          <RightArrow />
+          {
+            isPlaying
+              ? <Pause isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+              : <Play isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+          }
+          <LeftArrow />
+        </div>
+
+        {/* PLAYLIST */}
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-2 h-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
+          <h1 className="font-semibold text-xl px-6 mt-3">Playlist</h1>
+          <ul className="px-6 font-semibold text-gray-400">
+            <li className="flex justify-between items-center h-10 border-b">
+              <span>Blowin in the Wind</span>
+              <Play playlist={false} />
+            </li>
+            <li className="flex justify-between items-center h-10 border-b">
+              <span>Like a Rolling Stone</span>
+              <Play playlist={false} />
+            </li>
+            <li className="flex justify-between items-center h-10 border-b">
+              <span>Knockin on Heavens Door</span>
+              <Play playlist={false} />
+            </li>
+          </ul>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
