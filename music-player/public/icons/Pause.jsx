c70cb1aaebf859302d950b40864d129e51a5c591
Henny Feliz <mingito.2017@gmail.com>
2024-07-28 02:02:56 -0400

    icon components changes

--- music-player/public/icons/Pause.jsx
+++ music-player/public/icons/Pause.jsx
@@ -1,3 +1,3 @@
-export default function Pause({ isPlaying, setIsPlaying }) {
-  return <svg onClick={() => setIsPlaying(!isPlaying)} width="34px" height="34px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 5V19M16 5V19" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+export default function Pause({ isPlaying, setIsPlaying, dark }) {
+  return <svg onClick={() => setIsPlaying(!isPlaying)} width="34px" height="34px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 5V19M16 5V19" stroke={`${dark ? "#ffffff" : "#000000"}`} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
 }
