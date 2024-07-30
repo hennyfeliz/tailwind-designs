d3ce0fe6b97abc377d0a1fafdad485d213f2cf27
Henny Feliz <mingito.2017@gmail.com>
2024-07-30 09:21:36 -0400

    Controls and header components changes, adding some opcional rendering for theme config

--- music-player/src/components/Controls.jsx
+++ music-player/src/components/Controls.jsx
@@ -1,4 +1,4 @@
-const Controls = ({ thumb, song }) => {
+const Controls = ({ thumb, song, dark }) => {
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -13,7 +13,7 @@ const Controls = ({ thumb, song }) => {
 
   return (
     <div className="w-full h-8 px-4 items-center">
-      <input type="range" min="1" max={timeToSeconds(song.duration)} value={thumb} id="slider" />
+      <input className={`${dark ? "dark_" : "light_"}`} type="range" min="1" max={timeToSeconds(song.duration)} value={thumb} id="slider" />
       <div className="flex justify-between px-2 text-gray-500 font-extralight pt-1">
         <span>{formatTime(thumb)}</span>
         <span>{song.duration}</span>
--- music-player/src/components/Header.jsx
+++ music-player/src/components/Header.jsx
@@ -4,7 +4,7 @@ import SongIcon3 from "../../public/icons/SongIcon3"
 import Sound from "../../public/icons/Sound";
 import NotSound from "../../public/icons/NotSound";
 
-const Header = () => {
+const Header = ({ dark }) => {
 
   const [enableMusic, setEnableMusic] = useState(true);
   const [sound, setSound] = useState(false);
@@ -21,18 +21,19 @@ const Header = () => {
 
       {/* HEADER ICONS */}
       <div className="flex items-center space-x-1">
-        <div className={`p-1 hover:bg-gray-300 duration-200 rounded cursor-pointer ${enableMusic && "bg-gray-300"}`}>
+        <div className={`p-1 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"} duration-200 rounded cursor-pointer ${enableMusic ? (dark ? "bg-[#2e324b]" : "bg-gray-300") : ""}`}>
           {
             enableMusic
-              ? <SongIcon3 enableMusic={enableMusic} setEnableMusic={setEnableMusic} />
-              : <SongIcon1 enableMusic={enableMusic} setEnableMusic={setEnableMusic} />
+              ? <SongIcon3 dark={dark} enableMusic={enableMusic} setEnableMusic={setEnableMusic} />
+              : <SongIcon1 dark={dark} enableMusic={enableMusic} setEnableMusic={setEnableMusic} />
           }
         </div>
-        <div className={`p-1 hover:bg-gray-300 duration-200 rounded cursor-pointer ${sound && "bg-gray-300"}`}>
+        <div className={`p-1 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"} duration-200 rounded cursor-pointer 
+          ${sound ? (dark ? "bg-[#2e324b]" : "bg-gray-300") : ""}`}>
           {
             sound
-              ? <NotSound soundHandler={soundHandler} />
-              : <Sound soundHandler={soundHandler} />
+              ? <NotSound soundHandler={soundHandler} dark={dark} />
+              : <Sound soundHandler={soundHandler} dark={dark} />
           }
         </div>
       </div>
