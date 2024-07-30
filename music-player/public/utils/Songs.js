1effc8508bb4b2d0937053ec7d28163a04b096fe
Henny Feliz <mingito.2017@gmail.com>
2024-07-30 09:21:12 -0400

    Songs data array changes

--- music-player/public/utils/Songs.js
+++ music-player/public/utils/Songs.js
@@ -3,20 +3,24 @@ export default [
     name_: "Blowin in the Wind",
     artist: "Bob Dylan",
     duration: "2:48",
+    fav_: false,
   },
   {
     name_: "Like a Rolling Stone",
     artist: "Bob Dylan",
     duration: "6:13",
+    fav_: false,
   },
   {
     name_: "Knockin on Heavens D",
     artist: "Bob Dylan",
     duration: "2:30",
+    fav_: true,
   },
   {
     name_: "The Times They Are",
     artist: "Bob Dylan",
     duration: "3:15",
+    fav_: true,
   },
 ];
