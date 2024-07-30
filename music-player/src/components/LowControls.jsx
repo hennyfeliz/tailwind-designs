01298e038ef14bb3de1899203d1cb5fc94f4544a
Henny Feliz <mingito.2017@gmail.com>
2024-07-30 09:22:21 -0400

    Low controls and playlist components, adding some config and renderization rules

--- music-player/src/components/LowControls.jsx
+++ music-player/src/components/LowControls.jsx
@@ -3,7 +3,7 @@ import LeftArrow from "../../public/icons/LeftArrow"
 import Play from "../../public/icons/Play"
 import Pause from "../../public/icons/Pause"
 
-const LowControls = ({ isPlaying, setIsPlaying, setThumb, songIndex, setSongIndex }) => {
+const LowControls = ({ isPlaying, setIsPlaying, setThumb, songIndex, setSongIndex, dark, setDark }) => {
   return (
     <div className="flex w-full h-14 px-20 items-center justify-between">
 
@@ -13,16 +13,16 @@ const LowControls = ({ isPlaying, setIsPlaying, setThumb, songIndex, setSongInde
         songIndex !== 0
           ? setSongIndex(songIndex - 1)
           : setSongIndex(3)
-      }} className="p-1 hover:bg-gray-300 duration-200 rounded cursor-pointer">
-        <RightArrow />
+      }} className={`p-1 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"}  duration-200 rounded cursor-pointer`}>
+        <RightArrow dark={dark} setDark={setDark} />
       </div>
 
       {/* CENTRAL PLAY BUTTON */}
-      <div className="p-1 hover:bg-gray-300 duration-200 rounded cursor-pointer">
+      <div className={`p-1 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"} duration-200 rounded cursor-pointer`}>
         {
           isPlaying
-            ? <Pause isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
-            : <Play isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+            ? <Pause isPlaying={isPlaying} setIsPlaying={setIsPlaying} dark={dark} setDark={setDark} />
+            : <Play isPlaying={isPlaying} setIsPlaying={setIsPlaying} dark={dark} setDark={setDark} />
         }
       </div>
 
@@ -32,8 +32,8 @@ const LowControls = ({ isPlaying, setIsPlaying, setThumb, songIndex, setSongInde
         songIndex !== 3
           ? setSongIndex(songIndex + 1)
           : setSongIndex(0)
-      }} className="p-1 hover:bg-gray-300 duration-200 rounded cursor-pointer">
-        <LeftArrow />
+      }} className={`p-1 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"} duration-200 rounded cursor-pointer`}>
+        <LeftArrow dark={dark} setDark={setDark} />
       </div>
 
 
--- music-player/src/components/Playlist.jsx
+++ music-player/src/components/Playlist.jsx
@@ -1,7 +1,10 @@
-import Play from "../../public/icons/Play"
 import Songs from "../../public/utils/Songs"
+import PlaylistIcon from "../../public/icons/PlaylistIcon"
+import Heart from "../../public/icons/Heart"
+import { useEffect, useState } from "react"
 
-const Playlist = ({ resetSlider, showPlaylist, setShowPlaylist, setThumb, songIndex, setSongIndex }) => {
+const Playlist = ({ resetSlider, showPlaylist, setShowPlaylist, setThumb, songIndex, setSongIndex, dark }) => {
+  const [rerender, setRerender] = useState(false)
 
   const songIndexHandler = (index) => {
     setThumb(0);
@@ -9,11 +12,19 @@ const Playlist = ({ resetSlider, showPlaylist, setShowPlaylist, setThumb, songIn
     resetSlider();
   }
 
+  const heartHandler = (index) => {
+    Songs[index].fav_ = !Songs[index].fav_
+    setRerender(!rerender)
+  }
+
+  useEffect(() => {
+  }, [rerender])
+
   return (
     <div>
       <div className="flex flex-row items-center mt-3 justify-between px-8">
         <h1 className="font-semibold text-xl">Playlist</h1>
-        <div className="py-1 px-3 hover:bg-gray-300 duration-200 rounded cursor-pointer">
+        <div className={`py-1 px-3 duration-200 rounded cursor-pointer ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"}`}>
           <span onClick={() => {
             setShowPlaylist(!showPlaylist);
           }}>
@@ -24,12 +35,13 @@ const Playlist = ({ resetSlider, showPlaylist, setShowPlaylist, setThumb, songIn
       <ul className={`px-6 font-semibold text-gray-400 transition-all duration-200  ${showPlaylist ? "opacity-100 block" : "opacity-0 none"}`} >
         {
           Songs.map((song, index) => (
-            index !== songIndex ?
-              <li onClick={() => {
-                songIndexHandler(index);
-              }} key={index} className="flex hover:bg-gray-200 px-2 rounded justify-between items-center h-10 border-b cursor-pointer" >
+            index !== songIndex && song.fav_ === true ?
+              <li key={index} className={`flex duration-200 px-2 rounded justify-between items-center h-10  cursor-pointer ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"}`}>
                 <span>{song.name_}</span>
-                <Play playlist={false} />
+                <div className={`pr-2 space-x-2 ${dark ? "hover:bg-[#2e324b]" : "hover:bg-gray-300"} rounded transition-all flex items-center justify-center`}>
+                  <Heart fav={song.fav_} dark={dark} heartHandler={heartHandler} index={index} />
+                  <PlaylistIcon playlist={false} songIndexHandler={songIndexHandler} index={index} />
+                </div>
               </li>
               : null
           ))
