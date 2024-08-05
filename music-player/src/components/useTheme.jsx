e78011af368bf2d0a1f4f651528e3d44eb6db191
Henny Feliz <mingito.2017@gmail.com>
2024-08-04 23:54:08 -0400

    usetheme component

---
+++ music-player/src/components/useTheme.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+const useTheme = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const oldTheme = theme === 'dark' ? 'light' : 'dark';
+    root.classList.remove(oldTheme);
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return [theme, setTheme];
+};
+
+export default useTheme;
